34ffdf2ec72602d5b2abd4b2e99ae33defa45a61
anass17 <anassboutaib2018@gmail.com>
2025-03-12 13:15:12 +0000

    Added new color degrees

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
 
-import Navbar from "./components/navbar"
-
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+import Navbar from "./components/navbar"
+import HeroSection from './components/home/heroSection';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
   return (
     <>
       <Navbar background={false} />
+      <HeroSection />
     </>
   )
 }
--- Frontend/src/config/colors.ts
+++ Frontend/src/config/colors.ts
@@ -1,2 +1,5 @@
 export const DARK_GREEN : string = '#1DA68F';
+export const LIGHT_GREEN1 : string = '#27DEBF';
+export const LIGHT_GREEN2 : string = '#E1FAF6';
+export const DARK_GRAY0 : string = '#111827';
 export const DARK_GRAY2 : string = '#424242';
