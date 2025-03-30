cb160fea94c7da1af2e958269337729ced23f8b0
anass17 <anassboutaib2018@gmail.com>
2025-03-30 14:11:36 +0000

    Added Medicine Seeders

---
+++ Backend/database/seeders/MedicineSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MedicineSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('medicine_forms')->insert([
+            ['name' => 'Table', 'unit' => 'mg'],
+            ['name' => 'Capsule', 'unit' => 'mg'],
+            ['name' => 'Syrup', 'unit' => 'ml'],
+            ['name' => 'Cream', 'unit' => 'mg']
+        ]);
+
+        DB::table('medicine_uses')->insert([
+            ['name' => 'Fever'],
+            ['name' => 'Pain'],
+            ['name' => 'Infection'],
+            ['name' => 'Inflammation'],
+            ['name' => 'Allergy'],
+            ['name' => 'Cough'],
+            ['name' => 'Anxiety'],
+            ['name' => 'Depression'],
+            ['name' => 'Diabetes'],
+            ['name' => 'Insomnia'],
+            ['name' => 'Asthma'],
+        ]);
+    }
+}
