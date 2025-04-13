b72e0fcf0de909e151013a52741f19baa8a380b7
anass17 <anassboutaib2018@gmail.com>
2025-04-13 19:41:56 +0100

    Added New Colors

--- Frontend/src/config/colors.js
+++ Frontend/src/config/colors.js
@@ -5,6 +5,9 @@ export const GREEN2 = '#27DEBF';
 export const GREEN3 = '#BEF5EC';
 export const GREEN5 = '#E1FAF6';
 export const LIGHT_GREEN2 = '#E1FAF6';
+export const PRIMARY_GREEN = '#28a745'
+export const LIGHT_GREEN_ACCENT = '#d4edda';
+export const MUTED_GREEN = '#f0fdf4';
 
 
 
@@ -14,3 +17,10 @@ export const GRAY0 = '#111827';
 export const GRAY2 = '#424242';
 export const GRAY3 = '#585858';
 export const GRAY4 = '#D0D0D0';
+
+
+
+// Blue
+
+export const PRIMARY_BLUE = '#007BFF';
+export const LIGHT_BLUE = '#e6f0fb';
