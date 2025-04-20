68219bd46549f9b1e787cdfe47b0d8f2d35441bc
anass17 <anassboutaib2018@gmail.com>
2025-04-20 21:53:02 +0100

    DOC-187: Créer la table Appointements dans la base de données

---
+++ Backend/database/migrations/2025_04_20_204400_create_appointments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('appointment_date');
+            $table->enum('appointment_type', ['online', 'in-person']);
+            $table->float('appointment_price');
+            $table->string('appointment_description');
+            $table->unsignedBigInteger('client_id');
+            $table->unsignedBigInteger('doctor_id');
+            $table->foreign('client_id')->on('users')->references('id')->onDelete('cascade');
+            $table->foreign('doctor_id')->on('users')->references('id')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
