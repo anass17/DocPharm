986f0f5447c57423d813f7955788ef3eb8cf4066
anass17 <anassboutaib2018@gmail.com>
2025-04-21 12:24:19 +0100

    Create Doctor's Navbar

--- Frontend/src/components/Navbar/DoctorNavbar.jsx
+++ Frontend/src/components/Navbar/DoctorNavbar.jsx
@@ -1,39 +1,53 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+
+import Avatar from '@mui/material/Avatar';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Tooltip from '@mui/material/Tooltip';
+import {Tooltip as TLP} from 'antd'
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+
+import { FaHome, FaUser, FaCalendarCheck } from "react-icons/fa";
+import CartDrawer from '../Drawer/CartDrawer.jsx';
+
 
 import * as colors from '../../config/colors.js';
 import { Link } from 'react-router-dom';
 
-const pages = [['Doctor', '/'], ['Sign out', '/logout']];
+import { Badge } from 'antd';
+import { useSelector } from 'react-redux';
 
 function DoctorNavbar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [drawerOpen, setDrawerOpen] = useState(false)
+  const cart = useSelector(data => data.cart.cart)
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
   };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
     let bgColor;
     let textColor;
 
-    bgColor = colors.GRAY0;
-    textColor = "#FFF"
+    bgColor = '#FFF';
+    textColor = colors.GRAY0
 
 
   return (
-    <AppBar position="static" sx={{backgroundColor: bgColor, color: textColor, boxShadow: 'none', py: 1 }}>
+    <AppBar position="static" sx={{backgroundColor: bgColor, color: textColor, top: 0, left: 0, py: 1, boxShadow: '0px 0px 2px rgba(0, 0, 0, .2)'}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Typography
@@ -49,84 +63,128 @@ function DoctorNavbar() {
               textDecoration: 'none',
             }}
           >
-            <Link to="/">
-              <span style={{color: colors.GREEN}}>Doc</span><span style={{ color: textColor }}>Pharm</span>
+            <Link to="/doctor/dashboard" style={{ display: 'flex' }}>
+              <img className='h-[70px]' src='/public/images/logo/logo.png' />
             </Link>
             
-          </Typography>
-
-          {/* Mobile */}
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{ display: { xs: 'block', md: 'none' } }}
-            >
-              {pages.map((page, index) => (
-                <MenuItem key={index} onClick={handleCloseNavMenu}>
-                  <Link to={page[1]} style={{ textAlign: 'center', textTransform: 'capitalize' }}>{page[0]}</Link>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            <Link to="/">
-              <span style={{color: colors.GREEN}}>Doc</span><span style={{ color: textColor }}>Pharm</span>
-            </Link>
           </Typography>
           
           {/* Desktop */}
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
-            {pages.map((page, index) => (
-              <Link
-                key={index}
-                to={page[1]}
-                onClick={handleCloseNavMenu}
-                style={{ margin: '0 0.5rem', display: 'block', fontFamily: 'roboto', color: textColor, textTransform: 'capitalize', fontSize: '1rem' }}
+          <Box sx={{ ml: 'auto', display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+            <Box sx={{ display: 'flex', mr: 3, gap: '3px' }}>
+              
+
+              <TLP title="Dashboard" color={colors.PRIMARY_GREEN}>
+                <Link to={'/doctor/dashboard'}>
+                  <Button disableRipple sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <FaHome fill={colors.GRAY2} size={24} />
+                  </Button>
+                </Link>
+              </TLP>
+
+              <TLP title="Appointments" color={colors.PRIMARY_GREEN}>
+                <Badge count={0} size="small" offset={[-17, 5]} style={{background: '#FFF', color: colors.PRIMARY_GREEN, borderColor: colors.PRIMARY_GREEN, fontWeight: 500}}>
+                  <Link to={'/doctor/appointments'}>
+                    <Button disableRipple sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                      <FaCalendarCheck fill={colors.GRAY2} size={22} />
+                    </Button>
+                  </Link>
+                </Badge>
+              </TLP>
+
+              <TLP title="Profile" color={colors.PRIMARY_GREEN}>
+                <Link to={'/doctor/profile'}>
+                  <Button disableRipple sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}> 
+                    <FaUser fill={colors.GRAY2} size={22}/>
+                  </Button>
+                </Link>
+              </TLP>
+
+            </Box>
+            <Tooltip title="Account settings">
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
               >
-                {page[0]}
-              </Link>
-            ))}
+                <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+              </IconButton>
+            </Tooltip>
           </Box>
+          <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            slotProps={{
+              paper: {
+                elevation: 0,
+                sx: {
+                  minWidth: 200,
+                  overflow: 'visible',
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  '&::before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: 'background.paper',
+                    transform: 'translateY(-50%) rotate(45deg)',
+                    zIndex: 0,
+                  },
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          >
+            <Link to={'/doctor/profile'}>
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <FaUser fontSize="small" />
+                </ListItemIcon>
+                Profile
+              </MenuItem>
+            </Link>
+
+            <Link to={'/doctor/settings'}>
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <Settings fontSize="small" />
+                </ListItemIcon>
+                Settings
+              </MenuItem>
+            </Link>
+
+            <Link to={'/logout'}>
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <Logout fontSize="small" />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Link>
+
+          </Menu>
           
         </Toolbar>
       </Container>
+      <CartDrawer open={drawerOpen} setOpen={setDrawerOpen} />
     </AppBar>
   );
 }
