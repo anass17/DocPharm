1287183b1fc0cf9be57d9bfe5c25ebfb9b23481f
anass17 <anassboutaib2018@gmail.com>
2025-04-24 09:24:35 +0100

    Updated Loading style

--- Frontend/src/components/Loading/LoadingOverlay.jsx
+++ Frontend/src/components/Loading/LoadingOverlay.jsx
@@ -1,16 +1,14 @@
 import { LoadingOutlined } from "@ant-design/icons";
 import { GREEN2, GREEN3, LIGHT_BLUE } from "../../config/colors"
 import { Spin } from "antd";
+import '../../assets/style/CustomLoading.css'
 
 
 function LoadingOverlay() {
     return (
         <>
             <div style={{ position: 'absolute', top: 0, left: 0, zIndex: 1500, backgroundColor: LIGHT_BLUE, display: 'flex', gap: 20, justifyContent: 'center', alignItems: 'center', flexDirection: 'column', width: '100%', minHeight: '100vh'}}>
-                <img className="w-[100px]" src='/public/images/logo/icon.png' />
-                <div>
-                    <Spin indicator={<LoadingOutlined spin />} size="large" />
-                </div>
+                <span class="loader"></span>
             </div>
         </>
     )
