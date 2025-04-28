469a27f57ef96ea4477d007f60d7c6d8c00c57af
anass17 <anassboutaib2018@gmail.com>
2025-04-28 01:26:39 +0100

    Make pharmacy details page responsive

--- Frontend/src/Pages/user/sections/PharmacyViewSection.jsx
+++ Frontend/src/Pages/user/sections/PharmacyViewSection.jsx
@@ -166,85 +166,91 @@ const PharmacyViewSection = () => {
                 </Box>
             </Box>
             <Row gutter={[16, 24]}>
-                <Col span={6}>
+                <Col xs={24} lg={8} xl={6}>
                     <Box p={3} bgcolor='#FFF' mb={3} boxShadow='0px 1px 2px rgba(0, 0, 0, .2)' borderRadius={2}>
                         <Typography.Title level={4} style={{ marginBottom: 20 }}>About Us</Typography.Title>
                         <TP fontSize={14}>{loading ? 'loading...' : (pharmacy?.bio || "Not added")}</TP>
                     </Box>
 
-                    <Box p={3} bgcolor='#FFF' mb={3} boxShadow='0px 1px 2px rgba(0, 0, 0, .2)' borderRadius={2}>
-                        <Typography.Title level={4}>Working Hours</Typography.Title>
-                        {
-                            loading ? (
-                                'Loading...'
-                            ) : (
-                                pharmacy?.working_hours ? (
-                                    <>
-                                        <WorkingHoursLine day={'monday'} data={pharmacy?.working_hours} />
-                                        <WorkingHoursLine day={'tuesday'} data={pharmacy?.working_hours} />
-                                        <WorkingHoursLine day={'wednesday'} data={pharmacy?.working_hours} />
-                                        <WorkingHoursLine day={'thursday'} data={pharmacy?.working_hours} />
-                                        <WorkingHoursLine day={'friday'} data={pharmacy?.working_hours} />
-                                        <WorkingHoursLine day={'saturday'} data={pharmacy?.working_hours} />
-                                        <WorkingHoursLine day={'sunday'} data={pharmacy?.working_hours} />
-                                    </>
-                                ) : (
-                                    <TP fontSize={14}>Not Specified</TP>
-                                )
-                            )
-                        }
-                        
-                    </Box>
-                    <Box p={3} bgcolor='#FFF' boxShadow='0px 1px 2px rgba(0, 0, 0, .2)' borderRadius={2}>
-                        <Typography.Title level={4}>Contact Information</Typography.Title>
-                        <Box sx={{ mb: 2.5, fontWeight: 500 }}>
-                            <Typography.Text style={{ display: 'flex', gap: 15, padding: '0.3rem 0', alignItems: 'center' }}><FaPhoneAlt color={GREEN} fontSize={15} />{loading ? 'loading...' : pharmacy?.phone_number}</Typography.Text>
-                            <Typography.Text style={{ display: 'flex', gap: 15, padding: '0.3rem 0', alignItems: 'center' }}><FaEnvelope color={GREEN} fontSize={15} />{loading ? 'loading...' : pharmacy?.email}</Typography.Text>
-                            <Typography.Text style={{ display: 'flex', gap: 15, padding: '0.3rem 0', alignItems: 'center' }}><FaMapMarkerAlt color={GREEN} fontSize={15} />{loading ? 'loading...' : (pharmacy?.address + ' ' + pharmacy?.city)}</Typography.Text>
-                        </Box>
-                        <Box>
-                            <Typography.Text style={{ fontWeight: 500, marginBottom: 15, display: 'block' }}>Follow Us</Typography.Text>
-                            <Box style={{ display: 'flex', gap: 20 }}>
+                    <Row gutter={[20, 20]}>
+                        <Col xs={24} md={12} lg={24}>
+                            <Box p={3} bgcolor='#FFF' boxShadow='0px 1px 2px rgba(0, 0, 0, .2)' borderRadius={2}>
+                                <Typography.Title level={4}>Working Hours</Typography.Title>
                                 {
-                                    pharmacy?.facebook_url ? (
-                                        <Box sx={{ color: GRAY2, '&:hover': {color: '#1877F2'} }}>
-                                            <Link target="_blank" to={pharmacy.facebook_url} style={{ color: "inherit" }} >
-                                                <FaFacebook size={20} />
-                                            </Link>
-                                        </Box>
-                                    ) : null
-                                }
-                                {
-                                    pharmacy?.instagram_url ? (
-                                        <Box sx={{ color: GRAY2, '&:hover': {color: '#E1306C'} }}>
-                                            <Link target="_blank" to={pharmacy.instagram_url} style={{ color: "inherit" }}>
-                                                <FaInstagram size={20} />
-                                            </Link>
-                                        </Box>
-                                    ) : null
-                                }
-                                {
-                                    pharmacy?.twitter_url ? (
-                                        <Box sx={{ color: GRAY2, '&:hover': {color: '#1DA1F2'} }}>
-                                            <Link target="_blank" to={pharmacy.twitter_url} style={{ color: "inherit" }}>
-                                                <FaTwitter size={20} />
-                                            </Link>
-                                        </Box>
-                                    ) : null
+                                    loading ? (
+                                        'Loading...'
+                                    ) : (
+                                        pharmacy?.working_hours ? (
+                                            <>
+                                                <WorkingHoursLine day={'monday'} data={pharmacy?.working_hours} />
+                                                <WorkingHoursLine day={'tuesday'} data={pharmacy?.working_hours} />
+                                                <WorkingHoursLine day={'wednesday'} data={pharmacy?.working_hours} />
+                                                <WorkingHoursLine day={'thursday'} data={pharmacy?.working_hours} />
+                                                <WorkingHoursLine day={'friday'} data={pharmacy?.working_hours} />
+                                                <WorkingHoursLine day={'saturday'} data={pharmacy?.working_hours} />
+                                                <WorkingHoursLine day={'sunday'} data={pharmacy?.working_hours} />
+                                            </>
+                                        ) : (
+                                            <TP fontSize={14}>Not Specified</TP>
+                                        )
+                                    )
                                 }
                                 
                             </Box>
-                        </Box>
-                    </Box>
+                        </Col>
+                        <Col xs={24} md={12} lg={24}>
+                            <Box p={3} bgcolor='#FFF' height={'100%'} boxShadow='0px 1px 2px rgba(0, 0, 0, .2)' borderRadius={2}>
+                                <Typography.Title level={4}>Contact Information</Typography.Title>
+                                <Box sx={{ mb: 2.5, fontWeight: 500 }}>
+                                    <Typography.Text style={{ display: 'flex', gap: 15, padding: '0.3rem 0', alignItems: 'center' }}><FaPhoneAlt color={GREEN} fontSize={15} />{loading ? 'loading...' : pharmacy?.phone_number}</Typography.Text>
+                                    <Typography.Text style={{ display: 'flex', gap: 15, padding: '0.3rem 0', alignItems: 'center' }}><FaEnvelope color={GREEN} fontSize={15} />{loading ? 'loading...' : pharmacy?.email}</Typography.Text>
+                                    <Typography.Text style={{ display: 'flex', gap: 15, padding: '0.3rem 0', alignItems: 'center' }}><FaMapMarkerAlt color={GREEN} fontSize={15} />{loading ? 'loading...' : (pharmacy?.address + ' ' + pharmacy?.city)}</Typography.Text>
+                                </Box>
+                                <Box>
+                                    <Typography.Text style={{ fontWeight: 500, marginBottom: 15, display: 'block' }}>Follow Us</Typography.Text>
+                                    <Box style={{ display: 'flex', gap: 20 }}>
+                                        {
+                                            pharmacy?.facebook_url ? (
+                                                <Box sx={{ color: GRAY2, '&:hover': {color: '#1877F2'} }}>
+                                                    <Link target="_blank" to={pharmacy.facebook_url} style={{ color: "inherit" }} >
+                                                        <FaFacebook size={20} />
+                                                    </Link>
+                                                </Box>
+                                            ) : null
+                                        }
+                                        {
+                                            pharmacy?.instagram_url ? (
+                                                <Box sx={{ color: GRAY2, '&:hover': {color: '#E1306C'} }}>
+                                                    <Link target="_blank" to={pharmacy.instagram_url} style={{ color: "inherit" }}>
+                                                        <FaInstagram size={20} />
+                                                    </Link>
+                                                </Box>
+                                            ) : null
+                                        }
+                                        {
+                                            pharmacy?.twitter_url ? (
+                                                <Box sx={{ color: GRAY2, '&:hover': {color: '#1DA1F2'} }}>
+                                                    <Link target="_blank" to={pharmacy.twitter_url} style={{ color: "inherit" }}>
+                                                        <FaTwitter size={20} />
+                                                    </Link>
+                                                </Box>
+                                            ) : null
+                                        }
+                                        
+                                    </Box>
+                                </Box>
+                            </Box>
+                        </Col>
+                    </Row>
                 </Col>
-                <Col span={18} style={{ paddingTop: 10 }}>
+                <Col xs={24} lg={16} xl={18} style={{ paddingTop: 10 }}>
                     <Typography.Title level={2} style={{ textAlign: 'center', marginBottom: 45 }}>Pharmacy's Medicines</Typography.Title>
                     <Row gutter={[10, 10]}>
                         {
                             cardsLoading ? (
                                 Array(3).fill('').map((_, index) => {
                                     return (
-                                        <Col span={8} key={'medicine-' + index}>
+                                        <Col xs={24} sm={12} xl={8} key={'medicine-' + index}>
                                             <MedicineCardLoading />
                                         </Col>
                                     )
@@ -252,7 +258,7 @@ const PharmacyViewSection = () => {
                             ) : (
                                 medicines.map((item, index) => {
                                     return (
-                                        <Col span={8} key={'medicine-' + index}>
+                                        <Col xs={24} sm={12} xl={8} key={'medicine-' + index}>
                                             <UserMedicineCard medicine={item} />
                                         </Col>
                                     )
