28a332ee8690ac686005ec53186524e1ae1e59bd
anass17 <anassboutaib2018@gmail.com>
2025-04-29 01:30:24 +0100

    DOC-239: Ajouter une fonctionnalité pour suspendre les comptes

--- Frontend/src/components/Drawer/Users/UserDisplayDetailsDrawer.jsx
+++ Frontend/src/components/Drawer/Users/UserDisplayDetailsDrawer.jsx
@@ -1,20 +1,71 @@
 import React, { useState } from 'react';
-import { Col, Divider, Drawer, Flex, Image, Row, Tag, Typography } from 'antd';
+import { Col, Divider, Drawer, Flex, Image, notification, Row, Tag, Typography } from 'antd';
 import { Box, Button, Typography as TP } from '@mui/material';
 import { GRAY2, GRAY3, GREEN2, PRIMARY_BLUE, PRIMARY_GREEN } from '../../../config/colors';
-import { red } from '@mui/material/colors';
+import { grey, red } from '@mui/material/colors';
 import dayjs from 'dayjs'
 import { FaMapMarkerAlt, FaPhone, FaPhoneAlt } from 'react-icons/fa';
-import { backend_url } from '../../../config/app';
+import { backend_url } from "../../../config/app";
+import Cookies from 'js-cookie'
 import { Link } from 'react-router-dom';
 
 let {Title, Text} = Typography
 
 const UserDisplayDetailsDrawer = ({user, open, setOpen}) => {
 
+    const [loading, setLoading] = useState(false)
+    const [api, NotificationHolder] = notification.useNotification();
+
+    const openNotification = (message, description, status) => {
+        api.open({
+            message: message,
+            type: status,
+            description: <p>{description}</p>,
+            placement: 'bottomRight',
+            duration: 5,
+            showProgress: true,
+            pauseOnHover: true,
+        });
+    };
+
     const onClose = () => {
         setOpen(false);
     };
+
+    const handleBanUser = () => {
+        setUserStatus('banned')
+    }
+
+    const setUserStatus = async (status) => {
+        
+        setLoading(true)
+
+        try {
+
+            const response = await fetch(`${backend_url}/api/users/${user.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Authorization': 'Bearer ' + Cookies.get('auth_token'),
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify({ status: status })
+            });
+        
+            if (response.status === 401) {
+                openNotification('Access Denied', 'You are not authorized to view this data', 'error');
+            } else if (response.status === 204) {
+                openNotification('Success', 'User has been banned', 'success');
+                setOpen(false)
+            } else {
+                openNotification('Something went wrong!', 'Could not perform this action', 'error');
+            }
+        } catch (error) {
+            console.log(error)
+            openNotification('Something went wrong!', 'Could not perform this action', 'error');
+        } finally {
+            setLoading(false)
+        }
+    }
 
     if (!user) {
         return
@@ -22,6 +73,7 @@ const UserDisplayDetailsDrawer = ({user, open, setOpen}) => {
 
     return (
         <>
+            {NotificationHolder}
             <Drawer title={"Appointment Details"} size={'large'} onClose={onClose} open={open}>
                 <Typography.Title level={4} style={{ marginBottom: 25 }}>General Information</Typography.Title>
                 <Row gutter={[16, 20]} style={{ alignItems: 'center', marginBottom: 40 }}>
@@ -138,6 +190,17 @@ const UserDisplayDetailsDrawer = ({user, open, setOpen}) => {
                         </>
                     ) : null
                 }
+
+                <Divider />
+
+                {
+                    loading ? (
+                        <Button variant='contained' loading style={{ backgroundColor: grey[200] }}>Ban</Button>
+                    ) : (
+                        <Button variant='contained' style={{ px: 5, py: 10, display: 'block', backgroundColor: red[500] }} onClick={handleBanUser}>Ban</Button>
+                    )
+                }
+
 
                 
 
